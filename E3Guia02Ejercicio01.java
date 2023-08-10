863ef65de2ec1932dbdbff79f96a5b4802b0124a
albertoFalum <alberto.alum@outlook.com.ar>
2023-08-10 17:02:38 -0300

    Initial commit

---
+++ E3Guia02Ejercicio01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package e3guia02ejercicio01;
+
+/**
+ *
+ * @author BETO
+ */
+public class E3Guia02Ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
